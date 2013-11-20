6ccd1fda8e066127fee3841a263a991f1c231d67
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-11-20 19:28:53 +1300

    Move exception popup to ordinary popup mechanism

--- debugger.js
+++ debugger.js
@@ -1,44 +1,40 @@
 function examineException(exception) {
-    var div = $c('div');
-    div.classList.add('exception-popup');
-    var closeButton = $c('button');
-    closeButton.appendChild($t('Close'));
-    closeButton.addEventListener('click', function() {div.remove();});
-    div.appendChild(closeButton);
-    div.appendChild($t('Available stack frames:'));
-    var ol = $c('ol');
-    for (var i=0; i<exception.stackFrames.length; i++) {
-        var frame = exception.stackFrames[i];
-        var li = $c('li');
-        var vl = $c('dl');
-        var numVariables = 0;
-        frame.forEach(function(name, value) {
-            var dt = $c('dt');
-            dt.appendChild($t(name));
-            var dd = $c('dd');
-            dd.appendChild($t(getDebugString(value)));
-            vl.appendChild(dt);
-            vl.appendChild(dd);
-            numVariables++;
-        });
-        vl.style.display = 'none';
-        li.addEventListener('click', function() {
-            this.firstChild.remove();
-            if (this.lastChild.style.display == 'none') {
-                this.lastChild.style.display = 'block';
-                this.insertBefore($t('▾'), this.firstChild);
-            } else {
-                this.lastChild.style.display = 'none';
-                this.insertBefore($t('▸'), this.firstChild);
-            }
-        });
-        li.appendChild($t('▸'));
-        li.appendChild($t(frame.methodName + ' (' + numVariables + ')'));
-        li.appendChild(vl);
-        ol.appendChild(li);
-    }
-    div.appendChild(ol);
-    document.body.appendChild(div);
+    popupBox(function(div) {
+        div.classList.add('exception-popup');
+        div.appendChild($t('Available stack frames:'));
+        var ol = $c('ol');
+        for (var i=0; i<exception.stackFrames.length; i++) {
+            var frame = exception.stackFrames[i];
+            var li = $c('li');
+            var vl = $c('dl');
+            var numVariables = 0;
+            frame.forEach(function(name, value) {
+                var dt = $c('dt');
+                dt.appendChild($t(name));
+                var dd = $c('dd');
+                dd.appendChild($t(getDebugString(value)));
+                vl.appendChild(dt);
+                vl.appendChild(dd);
+                numVariables++;
+            });
+            vl.style.display = 'none';
+            li.addEventListener('click', function() {
+                this.firstChild.remove();
+                if (this.lastChild.style.display == 'none') {
+                    this.lastChild.style.display = 'block';
+                    this.insertBefore($t('▾'), this.firstChild);
+                } else {
+                    this.lastChild.style.display = 'none';
+                    this.insertBefore($t('▸'), this.firstChild);
+                }
+            });
+            li.appendChild($t('▸'));
+            li.appendChild($t(frame.methodName + ' (' + numVariables + ')'));
+            li.appendChild(vl);
+            ol.appendChild(li);
+        }
+        div.appendChild(ol);
+    });
 }
 
 function getDebugString(value) {
