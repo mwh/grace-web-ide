21526f0b1b8c953e734589da5fcd5b079278e49e
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-11-20 22:21:30 +1300

    Add callback for failed XHR

--- xhr.js
+++ xhr.js
@@ -1,10 +1,14 @@
-function getFile(url, callback) {
+function getFile(url, callback, failedCallback) {
     var req = new XMLHttpRequest();
     req.open("GET", url, true);
     req.onreadystatechange = function() {
         if (req.readyState == 4) {
             if (req.status == 200) {
                 callback(req.responseText);
+            } else {
+                if (failedCallback) {
+                    failedCallback(req.status);
+                }
             }
         }
     }
